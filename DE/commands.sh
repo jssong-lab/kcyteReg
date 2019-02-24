b78bbca1ed2b1d1278ae4384d0f0ee4430512054
A Finnegan <afinneg2@illinois.edu>
2019-02-23 22:02:13 -0600

    update DE/commands.sh

--- DE/commands.sh
+++ DE/commands.sh
@@ -9,14 +9,15 @@ set -euo pipefail
 
 #######################################
 ## Write cellNames_BKvsDK.txt
-#f_in="../clusterCells/kasp.ka10.k30_impute.t10_gene.geq5UMIgeq100Cell.all.csv"
-#l="./reindex_BKvsDK.txt"
-#fo=cellNames_BKvsDK.txt
-#
-#awk -v d=, -v f=22 'BEGIN{ FS=OFS=d }
-#NR==FNR {map[$1] = $2; next} 
-#{ if ($f in map) {$f = map[$f];  print } }' $l $f_in | cut -f1,22 -d , | sort -t "," -k 2,2n -k 1,1 > $fo
+f_in="../clusterCells/kasp.ka10.k30_impute.t10_gene.geq5UMIgeq100Cell.all.csv"
+l="./reindex_BKvsDK.txt"
+fo=cellNames_BKvsDK.txt
 
+awk -v d=, -v f=22 'BEGIN{ FS=OFS=d }
+NR==FNR {map[$1] = $2; next} 
+{ if ($f in map) {$f = map[$f];  print } }' $l $f_in | cut -f1,22 -d , | sort -t "," -k 2,2n -k 1,1 > $fo
+
+exit
 #######################################
 ## Write Rdata files for DE
 fi_rawCounts="../raw/rawCount_filtered_foreskin.kcyte.csv"
@@ -35,7 +36,6 @@ name="write_Rdata"
 
 Rscript ../codes/write-countdata.colData_includeGeneList.R --fi_rawCounts $fi_rawCounts --cellRows --fi_metaData $fi_metaData --cellNames $cellNames --geneNames $geneNames --thresh_UMI $thresh_UMI --thresh_cell $thresh_cell  --fo_countdata $fo_countData --fo_coldata $fo_colData 1> ./oe/${name}.o 2> ./oe/${name}.e
 
-exit
 #######################################
 ## Run DE 
 fi_countData="DE_countData.Rdata"
